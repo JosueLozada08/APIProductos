9750315274113c12ccebcba3139e64d4f675c544
josue <josue_lozadavillacis@hotmail.com>
2023-10-18 09:23:45 -0500

    Se agrega archivos

---
+++ APIProductos-master/APIProductos/Migrations/20231018140713_Tabla1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIProductos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tabla1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
